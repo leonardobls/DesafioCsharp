89265f3bf845e8ca311cbf089cfd1a7e2dfaef9c
Leonardo Bitencourt Lima da Silva <leonardo.lima@keyworks.com.br>
2024-11-14 08:30:04 -0300

    Implementação do algoritmo do exercício 2.

---
+++ KeyWayAlgoritmosDesafio2/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira os 2 valores: \n");
+        string input = Console.ReadLine() ?? "";
+
+        string[] values = input.Split(" ");
+
+        int firstNumber = 0, secondNumber = 0;
+
+        if ((values.Length < 2 || values.Length > 2) || !(int.TryParse(values[0], out firstNumber)) || !(int.TryParse(values[1], out secondNumber)))
+            Console.WriteLine("É necessário inserir 2 números, nada a mais, nada a menos!");
+        Environment.Exit(0);
+
+        double pointOfEnd = firstNumber % secondNumber;
+
+
+        Console.WriteLine($"{pointOfEnd}\n");
+    }
+}
