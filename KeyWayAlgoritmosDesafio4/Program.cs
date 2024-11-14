1b987ed377209fb162e5610abc3093cc0cfb29ce
Leonardo Bitencourt Lima da Silva <leonardo.lima@keyworks.com.br>
2024-11-14 14:14:32 -0300

    Implementação do algoritmo 4

---
+++ KeyWayAlgoritmosDesafio4/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine() ?? "";
+        if (int.TryParse(input, out int number))
+        {
+            List<int> factorials = [];
+            int factorial = 1, i = 1;
+
+            while (factorial <= number)
+            {
+                factorials.Add(factorial);
+                i++;
+                factorial *= i;
+            }
+
+            int count = 0;
+            int index = factorials.Count - 1;
+
+            while (number > 0)
+            {
+                if (factorials[index] <= number)
+                {
+                    number -= factorials[index];
+                    count++;
+                }
+                else
+                {
+                    index--;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
